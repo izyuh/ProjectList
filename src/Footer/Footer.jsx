4116bbbbe07b62b843532ac7271f282e4eb502d1
isaias <isaias.gutierrez48@gmail.com>
2024-11-13 08:49:52 -0800

    added vite build

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,18 +1,31 @@
 import React from "react";
-import './footer.scss'
+import "./footer.scss";
 
 function Footer() {
-    return <div className="footer">
-        <div className="contact-links">
-            <a href="mailto:isaias.gutierrez48@gmail.com" target="_blank">Email Logo Here</a>
+  return (
+    <div className="footer">
+      <div className="contact-links">
+        <a href="mailto:isaias.gutierrez48@gmail.com" target="_blank">
+          Email Logo Here
+        </a>
 
-            <a href="https://github.com/izyuh" target="_blank">GitHub Logo Here</a>
+        <a href="https://github.com/izyuh" target="_blank">
+          GitHub Logo Here
+        </a>
 
-            <a href="https://www.linkedin.com/in/isaias-isaiah-gutierrez-611aba1a3/" target="_blank">LinkedIn Logo Here</a>
+        <a
+          href="https://www.linkedin.com/in/isaias-isaiah-gutierrez-611aba1a3/"
+          target="_blank"
+        >
+          LinkedIn Logo Here
+        </a>
 
-            <a href="./public/Resume.pdf" target="_blank">Resume Logo Here</a>
-        </div>
+        <a href="./public/Resume.pdf" target="_blank">
+          Resume Logo Here
+        </a>
+      </div>
     </div>
+  );
 }
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import "./main.css";
 import Header from "./Header/Header.jsx";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
-import Skills from './Skills/Skills'
+import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
-import Footer from './Footer/Footer'
+import Footer from "./Footer/Footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
